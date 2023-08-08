4c4d23d6df9f00fe08ebc703df5b68f2bb533061
harvee <harvee0813@naver.com>
2023-08-08 20:56:03 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/yes48/Yes48Application.java
@@ -0,0 +1,13 @@
+package com.example.yes48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yes48Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yes48Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/yes48/Yes48ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yes48;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yes48ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
