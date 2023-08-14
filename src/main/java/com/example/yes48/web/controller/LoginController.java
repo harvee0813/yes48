f60e920e99e80383bbb1065bcfa693f6ed4618f9
harvee <harvee0813@naver.com>
2023-08-14 16:10:26 +0900

    login 제작

---
+++ src/main/java/com/example/yes48/web/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.yes48.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "client/login";
+    }
+}
