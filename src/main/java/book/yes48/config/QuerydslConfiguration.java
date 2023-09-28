6d128d01b0041d5e40f94c2cd370d23ba8790a7e
harvee <harvee0813@naver.com>
2023-09-28 09:51:23 +0900

    build.gradle 수정

--- src/main/java/book/yes48/config/QuerydslConfiguration.java
+++ src/main/java/book/yes48/config/QuerydslConfiguration.java
@@ -5,6 +5,12 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.query.JpaQueryMethod;
+import org.springframework.data.jpa.repository.query.JpaQueryMethodFactory;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.repository.core.RepositoryMetadata;
+
+import java.lang.reflect.Method;
 
 /**
  * 매번 사용하기 때문에 config로 작성
